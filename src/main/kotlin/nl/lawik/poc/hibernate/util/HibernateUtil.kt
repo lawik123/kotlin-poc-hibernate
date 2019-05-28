b6acbb3eb2bc68b5c765f1a021f2141f5cf8c757
Lawik <428302@student.saxion.nl>
2019-05-28 14:59:27 +0200

    Added proper transaction handling.

--- src/main/kotlin/nl/lawik/poc/hibernate/util/HibernateUtil.kt
+++ src/main/kotlin/nl/lawik/poc/hibernate/util/HibernateUtil.kt
@@ -5,6 +5,7 @@ import org.hibernate.SessionFactory
 import org.hibernate.boot.MetadataSources
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder
 import org.hibernate.query.Query
+import java.lang.Exception
 import javax.persistence.criteria.*
 import kotlin.reflect.KProperty1
 
@@ -21,6 +22,8 @@ object HibernateUtil {
  * Helper for creating a session scope and provides it to the provided function literal.
  * The session is closed after the function literal is processed
  * the result from the function literal is returned.
+ * Starts and commits a transaction for the session scope if the handler is processed successfully, rollsback
+ * the transaction if an exception occurs.
  *
  * @param handler function literal that makes use of the session, you can access the session using the "it" keyword
  * in the function literal.
@@ -29,13 +32,21 @@ object HibernateUtil {
  */
 fun <T> openAndCloseSession(handler: (Session) -> T): T {
     val session = HibernateUtil.sessionFactory.openSession()
-    val res = handler(session)
-    if (session.isOpen) {
-        session.close()
-    } else {
-        println("[WARNING]: session closed by handler function: ${handler::class.java.enclosingMethod}, please consider omitting manually closing the session in the handler as it will be closed automatically at the end of the call")
+    session.beginTransaction()
+    return try {
+        val handlerResult = handler(session)
+        session.transaction.commit()
+        handlerResult
+    } catch (e: Exception) {
+        session.transaction.rollback()
+        throw e
+    } finally {
+        if (session.isOpen) {
+            session.close()
+        } else {
+            println("[WARNING]: session closed by handler function: ${handler::class.java.enclosingMethod}, please consider omitting manually closing the session in the handler as it will be closed automatically at the end of the call")
+        }
     }
-    return res
 }
 
 // extension functions
